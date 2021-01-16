0f30b5552b3efaca5b76a8294953b02f44b75dcb
Hayley Song <haejinso@usc.edu>
2021-01-16 14:02:34 -0800

    Adds a function to compute avg kld from a batch of mu's and logvar's

--- src/models/plmodules/utils.py
+++ src/models/plmodules/utils.py
@@ -1,6 +1,29 @@
 from pprint import pprint
+import torch
 import pytorch_lightning as pl
 from pytorch_lightning.utilities.cloud_io import load as pl_load
+
+
+def compute_kld(mu: torch.Tensor, logvar: torch.Tensor) -> torch.Tensor:
+    """
+    Compute the expected/averaged kl-divergence from a batch of mu's and logvar's
+    for a fully-factorized Gaussian distribution.
+
+    Assume dim0 is the batch dimension, and all the rest are data dimension.
+
+    .. note:: this function handles mu,logvar tensors whose dim > 2 (eg. mu_z maintains latent factors
+    for each channel, and thus have a shape of (nChannel=3, embedding_height=10, embedding_width=10).
+
+    :param mu: (BS, ...)
+    :param logvar: (BS, ...)
+    :return: a float tensor of average  kl-divergence per datapoint
+    """
+    batch_dim = 0
+    data_dims = tuple(range(1, mu.ndim))
+    kld = torch.mean(-0.5 * torch.sum(1 + logvar - mu ** 2 - logvar.exp(), dim=data_dims),
+                     dim=batch_dim)
+    return kld
+
 
 def get_best_ckpt(model: pl.LightningModule,
                   verbose:bool = False):
