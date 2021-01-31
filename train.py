808777f431917e5cd7fb30386e68e911049feca4
Hayley Song <haejinso@usc.edu>
2021-01-30 16:49:50 -0800

    renames

--- train.py
+++ train.py
@@ -1,79 +1,80 @@
-"""
-trainer_main.py
+"""train_bivae.py
+Train a single configuration of a model specified on a specified data
 
-Required args:
-    --model_name: "vae" or "iwae"
-    --data_name: "maptiles" or "mnist"
+Required CLI args
+-----------------
+    --model_name: "vae", "beta_vae", "iwae", "bivae"
+    --data_name: "mnist", "multi_rotated_mnist", "multi_mono_mnist",
+                "maptiles", "multi_maptiles"
     --latent_dim: int, eg. 10
 
-Optional args: (partial)
+Optional CLI args: (partial)
+----------------------------
     --hidden_dims: eg. --hidden_dims 32 64 128 256 (which is default)
 
+Note
+----
+  Each model (specified by --model_name) and datamodule (specified by --data_name)
+expects a different set of arguments. For example, `bivae` models allow the following
+arguments:
+Required:
+--n_styles
+--adversary_dim
+--adv_loss_weight
+
+
 To run: (at the root of the project, ie. /data/hayley-old/Tenanbaum2000
-nohup python src/train.py --model_name="vae" --data_name="mnist" --latent_dim=10
-nohup python src/train.py --model_name="iwae" --data_name="mnist" --latent_dim=10
+nohup python train.py --model_name="vae" --data_name="mnist" --latent_dim=10
+nohup python train.py --model_name="iwae" --data_name="mnist" --latent_dim=10
 
-Jan 23, 2021
-nohup python train.py --model_name="vae" --data_name="maptiles" --latent_dim=10 \
---cities berlin rome \
---styles StamenTonerBackground \
---zooms 14 \
---in_shape 1 64 64 \
---batch_size 32 \
---hidden_dims 32 32 64 64 128 \
---gpu_id 2 \
+
+# Train BetaVAE on MNIST
+nohup python train.py --model_name="beta_vae" \
+--enc_type "conv" --dec_type "conv" \
+--latent_dim=10 --hidden_dims 32 64 128 256 \
+--kld_weight=1.0  --use_beta_scheduler \
+--data_name="mnist" --data_root='/data/hayley-old/Tenanbaum2000/data' \
+-lr 3e-4 -bs 32 \
+--gpu_id=1 --max_epochs=200   --terminate_on_nan=True  \
+--log_root="/data/hayley-old/Tenanbaum2000/lightning_logs/2021-01-30/" &
+
+
+# Train BiVAE on Multi Monochrome MNIST
+nohup python train.py --model_name="bivae" \
+--latent_dim=10 --hidden_dims 32 64  --adv_dim 32 32 -lr 1e-3 --adv_weight 15.0 \
+--data_name="multi_mono_mnist" --colors red green blue --n_styles=3 \
+--gpu_id=1
+
+
+# Train BiVAE on Multi Rotated MNIST
+nohup python train.py --model_name="bivae" \
+--latent_dim=10 --hidden_dims 32 64 128 256 --adv_dim 32 32 32 \
+--data_name="multi_rotated_mnist" --angles -45 0 45 --n_styles=3 \
+--gpu_id=1
+
+nohup python train.py --model_name="bivae" \
+--latent_dim=10 --hidden_dims 32 64 128 256 --adv_dim 32 32 32 \
+--data_name="multi_rotated_mnist" --angles -45 0 45 --n_styles=3 \
+--gpu_id=2 --max_epochs=400   --terminate_on_nan=True  \
+-lr 3e-4 --adv_weight 15.0 \
 --log_root="/data/hayley-old/Tenanbaum2000/lightning_logs/2021-01-23/" &
 
 
-
-nohup python train.py --model_name="vae" --data_name="maptiles" --latent_dim=10 \
---cities 'la' 'charlotte' 'vegas' 'boston' 'paris' 'amsterdam' 'shanghai' 'seoul' 'chicago' 'manhattan' 'berlin' 'montreal' 'rome' \
---styles StamenTonerBackground \
+# Train BiVAE on Multi Maptiles MNIST
+nohup python train.py --model_name="bivae" \
+--latent_dim=10 --hidden_dims 32 64 128 256 --adv_dim 32 32 32 --adv_weight 15.0 \
+--data_name="multi_maptiles" \
+--cities la paris \
+--styles CartoVoyagerNoLabels StamenTonerBackground --n_styles=3 \
 --zooms 14 \
---in_shape 1 64 64 \
---batch_size 32 \
---hidden_dims 32 32 64 64 128 \
---gpu_id 2 \
+--gpu_id=2 --max_epochs=400   --terminate_on_nan=True  \
+-lr 3e-4 -bs 32 \
 --log_root="/data/hayley-old/Tenanbaum2000/lightning_logs/2021-01-23/" &
 
 
-nohup python train.py --model_name="vae" --data_name="maptiles" --latent_dim=10 \
---cities 'la' 'charlotte' 'vegas' 'boston' 'paris' 'amsterdam' 'shanghai' 'seoul' 'chicago' 'manhattan' 'berlin' 'montreal' 'rome' \
---styles StamenTonerBackground \
---zooms 14 \
---in_shape 1 64 64 \
---batch_size 32 \
---hidden_dims 32 64 64 64 128 \
---gpu_id 2 \
---log_root="/data/hayley-old/Tenanbaum2000/lightning_logs/2021-01-23/" &
-
-
-nohup python train.py --model_name="vae" --data_name="maptiles" --latent_dim=10 \
---cities 'la' 'charlotte' 'vegas' 'boston' 'paris' 'amsterdam' 'shanghai' 'seoul' 'chicago' 'manhattan' 'berlin' 'montreal' 'rome' \
---styles StamenTonerBackground \
---zooms 14 \
---in_shape 1 64 64 \
---batch_size 32 \
---hidden_dims 32 64 64 128 256 \
---gpu_id 1 \
---log_root="/data/hayley-old/Tenanbaum2000/lightning_logs/2021-01-23/" &
-
-nohup python train.py --model_name="vae" --data_name="maptiles" --latent_dim=20 \
---cities 'la' 'charlotte' 'vegas' 'boston' 'paris' 'amsterdam' 'shanghai' 'seoul' 'chicago' 'manhattan' 'berlin' 'montreal' 'rome' \
---styles StamenTonerBackground \
---zooms 14 \
---in_shape 1 64 64 \
---batch_size 32 \
---hidden_dims 32 64 64 128 256 \
---gpu_id 1 \
---log_root="/data/hayley-old/Tenanbaum2000/lightning_logs/2021-01-23/" &
 
 
 """
-# Load libraries
-# In[2]:
-
-
 import os,sys
 from datetime import datetime
 import time
@@ -81,41 +82,42 @@ from argparse import ArgumentParser
 from collections import OrderedDict
 from pathlib import Path
 from typing import List, Set, Dict, Tuple, Optional, Iterable, Mapping, Union, Callable, TypeVar
+import warnings
 from pprint import pprint
 
 import pytorch_lightning as pl
 from pytorch_lightning import loggers as pl_loggers
 
+
 # callbacks
 from src.callbacks.recon_logger import ReconLogger
 from src.callbacks.hist_logger import  HistogramLogger
+from pytorch_lightning.callbacks import LearningRateMonitor
+from src.callbacks.beta_scheduler import BetaScheduler
 
 # src helpers
 from src.utils.misc import info
 from src.models.model_wrapper import ModelWrapper
+from src.utils.misc import info, n_iter_per_epoch
 
 # utils for instatiating a selected datamodule and a selected model
-from .utils import get_model_class, get_dm_class
-from .utils import instantiate_model, instantiate_dm
+from utils import get_model_class, get_dm_class
+from utils import instantiate_model, instantiate_dm
 
 
 if __name__ == '__main__':
-
     parser = ArgumentParser()
-
     # Define general arguments for this CLI script for trianing/testing
     parser.add_argument("--model_name", type=str, required=True)
     parser.add_argument("--data_name", type=str, required=True)
+    parser.add_argument("--gpu_id", type=str, required=True, help="ID of GPU to use")
     parser.add_argument("--mode", type=str, default='fit', help="fit or test")
     parser.add_argument("--log_root", type=str, default='./lightning_logs', help='root directory to save lightning logs')
-    parser.add_argument("--gpu_id", type=str, required=True, help="ID of GPU to use")
 
     # Callback args
-    parser.add_argument("--hist_epoch_interval", type=int, default=10, help="Epoch interval to plot histogram of q's parameter")
-    parser.add_argument("--recon_epoch_interval", type=int, default=10, help="Epoch interval to plot reconstructions of train and val samples")
-
+    # parser.add_argument("--hist_epoch_interval", type=int, default=10, help="Epoch interval to plot histogram of q's parameter")
+    # parser.add_argument("--recon_epoch_interval", type=int, default=10, help="Epoch interval to plot reconstructions of train and val samples")
     parser.add_argument("-v", "--verbose", action="store_true", default=False)
-
     args, unknown = parser.parse_known_args()
     print("CLI args: ")
     pprint(args)
@@ -128,10 +130,13 @@ if __name__ == '__main__':
     parser = model_class.add_model_specific_args(parser)
     parser = dm_class.add_model_specific_args(parser)
     parser = pl.Trainer.add_argparse_args(parser)
+    # Callback switch args
+    parser = BetaScheduler.add_argparse_args(parser)
 
     args = parser.parse_args()
     print("Final args: ")
     pprint(args)
+
     # ------------------------------------------------------------------------
     # Initialize model, datamodule, trainer using the parsered arg dict
     # ------------------------------------------------------------------------
@@ -144,22 +149,39 @@ if __name__ == '__main__':
     dm.setup('fit')
     model = instantiate_model(args)
 
-    # Specify logger and callbacks
+    # Specify logger
     exp_name = f'{model.name}_{dm.name}'
     print('Exp name: ', exp_name)
     tb_logger = pl_loggers.TensorBoardLogger(save_dir=args.log_root,
                                              name=exp_name,
                                              default_hp_metric=False,
                                              )
+    log_dir = Path(tb_logger.log_dir)
+    print("Log Dir: ", log_dir)
+    # breakpoint()
+    if not log_dir.exists():
+        log_dir.mkdir(parents=True)
+        print("Created: ", log_dir)
+
+    # Specify callbacks
     callbacks = [
-        HistogramLogger(hist_epoch_interval=args.hist_epoch_interval),
-        ReconLogger(recon_epoch_interval=args.recon_epoch_interval),
+        LearningRateMonitor(logging_interval='epoch')
+        # HistogramLogger(hist_epoch_interval=args.hist_epoch_interval),
+        # ReconLogger(recon_epoch_interval=args.recon_epoch_interval),
         #         EarlyStopping('val_loss', patience=10),
     ]
+    if args.use_beta_scheduler:
+        max_iters = n_iter_per_epoch(dm.train_dataloader()) * args.max_epochs
+        callbacks.append(BetaScheduler(max_iters,
+                                       start=args.beta_start,
+                                       stop=args.beta_stop,
+                                       n_cycle=args.beta_n_cycle,
+                                       ratio=args.beta_ratio,
+                                       log_tag=args.beta_log_tag))
 
-    overwrites = {
-        'gpus':1,
-         'progress_bar_refresh_rate':0,
+    trainer_overwrites = {
+        'gpus':1, #use a single gpu
+        'progress_bar_refresh_rate':0, # don't print out progress bar
         'terminate_on_nan':True,
         'check_val_every_n_epoch':10,
         'logger': tb_logger,
@@ -167,12 +189,13 @@ if __name__ == '__main__':
     }
 
     # Init. trainer
-    trainer = pl.Trainer.from_argparse_args(args, **overwrites)
+    trainer = pl.Trainer.from_argparse_args(args, **trainer_overwrites)
 
     # Log model's computational graph
     model_wrapper = ModelWrapper(model)
     # tb_logger.experiment.add_graph(model_wrapper, model.)
     tb_logger.log_graph(model_wrapper)
+
 
     # ------------------------------------------------------------------------
     # Run the experiment
