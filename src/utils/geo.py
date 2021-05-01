0729561648579e15f4e59f261069621ca4a35aaf
Hayley Song <haejinso@usc.edu>
2021-05-01 12:28:49 -0700

    Working geo util functions -- I'm about to fix getTileExtent function

--- src/utils/geo.py
+++ src/utils/geo.py
@@ -2,11 +2,50 @@ import math
 from geopy.geocoders import Nominatim
 from typing import Tuple
 
+
+def deg2rad(x):
+	"""Convert the unit of x from degree to radian"""
+	return x * math.pi/180.0
+
+def getTileFromGeo(lat_deg, lng_deg, zoom):
+	'''
+	get tile index from geo location
+	:type : float, float, int
+	:rtype: tuple(int, int, int)
+	'''
+	x = math.floor((lng_deg + 180) / 360.0 * (2.0 ** zoom))
+
+	tan_y = math.tan(lat_deg * (math.pi / 180.0))
+	cos_y = math.cos(lat_deg * (math.pi / 180.0))
+	y = math.floor( (1 - math.log(tan_y + 1.0 / cos_y) / math.pi) / 2.0 * (2.0 ** zoom) )
+
+	return int(x), int(y), int(zoom)
+
 def getGeoFromTile(x, y, zoom):
 	lon_deg = x / (2.0 ** zoom) * 360.0 - 180.0
 	lat_rad = math.atan(math.sinh(math.pi * (1 - 2 * y / (2.0 ** zoom))))
 	lat_deg = lat_rad * (180.0 / math.pi)
 	return lat_deg, lon_deg
+
+def getTileExtent(x, y, zoom):
+	"""Computes the horizontal and vertical distance covered by the tile of size 256x256 (in meters)
+
+    Assumes tile size 256x256
+    Returns
+    -------
+    - (extent_y, exent_x): extent of the region coverage in latitudal and longitudal distance (meter)
+    Ref: "Distance per pixel math" in https://wiki.openstreetmap.org/wiki/Zoom_levels
+
+    """
+	lat_deg, lon_deg = getGeoFromTile(x,y,zoom)
+	lat_rad, lon_rad = deg2rad(lat_deg), deg2rad(lon_deg)
+
+	C_meters = 2*math.pi*6378137 # equatorial circumference of the Earth in meters
+	# size_tile = C_meters *
+
+	size_y = C_meters * math.cos(lat_rad)/2**zoom
+	size_x = C_meters * math.cos(lon_rad)/2**zoom
+	return size_y, size_x
 
 def getCountryFromTile(x,y,zoom) -> str:
 	"""Given x,y,z tile coords, return cityname"""
